a3cb19efb9bd4e71d85178c949aaa17b26e42784
Olena <kryvtsovaolena@gmail.com>
2022-10-20 20:22:47 +0300

    1
    first attempt

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import { Profile } from 'components/Profile/Profile';
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+      // username={user.username}
+      // tag={user.tag}
+      // location={user.location}
+      // avatar={user.avatar}
+      // stats={user.stats}
+      />
+    </div>
+  );
+};
